68c3e2edb95414c76b4802aa7aa2f9d6a050a0e2
Sergey Tkachuk <sergey.tkachuk@eltex.loc>
2025-08-08 12:27:19 +0300

    ver ++ 1.02

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.krayapp.xiaomidndfixer"
         minSdk = 29
         targetSdk = 35
-        versionCode = 101
-        versionName = "1.01"
+        versionCode = 102
+        versionName = "1.02"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
